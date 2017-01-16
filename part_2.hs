e2f8ac732dfbeedb8d233d7af291cb7458a19a8e
Christian Sakai <christianmsakai@gmail.com>
2017-01-16 05:41:57 -0500

    Finish Part 2 and Part 3

---
+++ part_2.hs
@@ -0,0 +1,32 @@
+-- Code
+double :: Num a => a -> a
+double x = x + x
+
+quadruple :: Num a => a -> a
+quadruple x = double (double x)
+
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1..n]
+
+average ns = sum ns `div` length ns
+
+-- Exercise
+-- No. 3
+n = a `div` length xs
+    where
+      a  = 10
+      xs = [1, 2, 3, 4, 5]
+
+-- No. 4
+last' :: [a] -> a
+last' xs = head (reverse xs)
+
+last'' :: [a] -> a
+last'' xs = xs !! (length xs - 1)
+
+-- No. 5
+init' :: [a] -> [a]
+init' xs = take (length xs - 1) xs
+
+init'' :: [a] -> [a]
+init'' xs = reverse . tail . reverse $ xs
---
+++ part_3.hs
@@ -0,0 +1,48 @@
+-- Code
+add :: (Int, Int) -> Int
+add (x, y) = x + y
+
+zeroto :: Int -> [Int]
+zeroto n = [0..n]
+
+add' :: Int -> (Int -> Int)
+add' x y = x + y
+
+mult :: Int -> (Int -> (Int -> Int))
+mult x y z = x * y * z
+
+-- Exercise
+-- No. 2
+bools :: [Bool]
+bools = [True, False]
+
+nums :: [[Int]]
+nums = [[1, 2], [3, 4]]
+
+add'' :: Int -> Int -> Int -> Int
+add'' x y z = x + y + z
+
+copy :: a -> (a, a)
+copy x = (x, x)
+
+apply :: (a -> b) -> a -> b
+apply f x = f x
+
+-- No. 3
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+pair :: a-> b -> (a, b)
+pair x y = (x, y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
