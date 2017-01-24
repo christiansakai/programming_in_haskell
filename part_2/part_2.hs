c7acb309e96ee1efc97c33eef72b483b0ac00e03
Christian Sakai <christianmsakai@gmail.com>
2017-01-23 22:36:36 -0500

    Revisit and finish part 2

--- part_2/part_2.hs
+++ part_2/part_2.hs
@@ -1,13 +1,15 @@
 -- Code
-double :: Num a => a -> a
+-- 2.5 Haskell scripts
+double :: (Num a) => a -> a
 double x = x + x
 
-quadruple :: Num a => a -> a
+quadruple :: (Num a) => a -> a
 quadruple x = double (double x)
 
 factorial :: (Num a, Enum a) => a -> a
 factorial n = product [1..n]
 
+average :: [Int] -> Int
 average ns = sum ns `div` length ns
 
 -- Exercise
