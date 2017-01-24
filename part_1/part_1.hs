2587db32063b1f8c447dd29b8d95358252382c4d
Christian Sakai <christianmsakai@gmail.com>
2017-01-23 22:33:24 -0500

    Revisit and finish part 1

--- part_1/part_1.hs
+++ part_1/part_1.hs
@@ -1,12 +1,14 @@
 -- Code
-double :: Num a => a -> a
+-- 1.1 Functions
+double :: (Num a) => a -> a
 double x = x + x
 
-sum' :: Num a => [a] -> a
+-- 1.5 A Taste of Haskell
+sum' :: (Num a) => [a] -> a
 sum' [x]        = x
 sum' (x:xs)     = x + sum' xs
 
-qsort' :: Ord a => [a] -> [a]
+qsort' :: (Ord a) => [a] -> [a]
 qsort' []     = []
 qsort' (x:xs) = qsort' smaller ++ [x] ++ qsort' larger
                 where
@@ -21,13 +23,17 @@ seqn' (act:acts)  = do
   return (x: xs)
 
 -- Exercise
+-- No. 1
+quadruple :: (Num a) => a -> a
+quadruple x = double (double x) 
+
 -- No. 3
-product' :: Num a => [a] -> a
+product' :: (Num a) => [a] -> a
 product' [x]     = x
 product' (x:xs)  = x * product' xs
 
 -- No. 4
-qsortReserve :: Ord a => [a] -> [a]
+qsortReserve :: (Ord a) => [a] -> [a]
 qsortReserve []     = []
 qsortReserve (x:xs) = qsortReserve larger ++ [x] ++ qsortReserve smaller
                       where
